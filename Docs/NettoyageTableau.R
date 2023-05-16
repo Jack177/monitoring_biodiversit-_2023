7407bd10d4aedb554fd24d6e08da9fa3ebbdba1a
Jordan Benrezkallah <jordan.benrezkallah@student.umons.ac.be>
2023-05-16 15:12:08 +0200

    auth Google

--- Docs/NettoyageTableau.R
+++ Docs/NettoyageTableau.R
@@ -1,30 +1,30 @@
 # Indiquer le dossier de travail
 setwd("C:/Users/User/Desktop")
 
-# Quelques packages de base pour traiter les données
+# Quelques packages de base pour traiter les donnees
 library(dplyr)
 library(readxl)
 library(tidyr)
 
 # Importation du tableau
-Inventaire <- read_excel("Inventaire monitoring 2023.xlsx")
+#Inventaire <- read_excel("Inventaire monitoring 2023.xlsx")
 
+Inventaire <- df
+# Identification e la famille quand le genre est inconnu :
 
-# Identification à la famille quand le genre est inconnu :
-
-Inventaire$Taxo = Inventaire$Genre_espece #On duplique la colonne qui porte le nom des espèces
+Inventaire$Taxo = Inventaire$Genre_espece #On duplique la colonne qui porte le nom des especes
 Inventaire[Inventaire$Genre_espece == "_","Taxo"]<- Inventaire[Inventaire$Genre_espece == "_","Famille"] #On remplace tous les vides par les noms de famille
 
 
-# Richesse spécifique par groupe taxonomique
+# Richesse specifique par groupe taxonomique
 
-Div<-Inventaire%>%filter(Station!="")  %>% group_by (Station,`Type de milieu`,`Groupe taxonomique` ) %>% summarise("Diversité" = length(unique(Taxo)))%>% pivot_wider( names_from = "Groupe taxonomique",values_from = "Diversité")%>% replace(is.na(.), 0)
+Div<-Inventaire%>%filter(Station!="")  %>% group_by (Station,`Type de milieu`,`Groupe taxonomique` ) %>% summarise("Diversite" = length(unique(Taxo)))%>% pivot_wider( names_from = "Groupe taxonomique",values_from = "Diversite")%>% replace(is.na(.), 0)
 
-# Présence absence, pour tous les groupes
+# Presence absence, pour tous les groupes
 
-Communaute <-Inventaire%>%filter(Station!="")  %>% group_by (Station,`Type de milieu`, Taxo) %>% summarise("Présence" = 1) %>% pivot_wider( names_from = "Taxo",values_from = "Présence")%>% replace(is.na(.), 0)
+Communaute <-Inventaire%>%filter(Station!="")  %>% group_by (Station,`Type de milieu`, Taxo) %>% summarise("Presence" = 1) %>% pivot_wider( names_from = "Taxo",values_from = "Presence")%>% replace(is.na(.), 0)
 
-# Indices de diversité
+# Indices de diversite
 
 Index <- Div[,1:2]
 library(vegan)
